09ae979b4c788aee6328ce00aabca47a50aad0e9
root <root@localhost.localdomain>
2019-06-10 00:19:52 +0000

    Added Jenkinsfile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Hi Jay
+
+
+
+What Else We Could Do
+There are a few other things we could have done to improve things, including:
+    . Plaing webserver under AWS NLB (Load Balancer) with autoscaling for High Availablity
+    . Use Docker Swarm / Kubernets for rolling update without downtime ( No need of stop/start containers) 
+    . Start tagging images with specific version to match a GitHub branch or tag
+    . Create an SSL certificate to serve over HTTPS
+    . <a
+          className="App-link"
+          href="https://github.com/jayasimha537/nodejs-my-app.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repo of this project
+        </a>
+
+
+
         </p>
         <a
           className="App-link"
